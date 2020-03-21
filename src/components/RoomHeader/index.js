225bae305e14e6543832368c4fe4a707e5c91ca3
Himawan Djaya Hendra <himawandh@gmail.com>
2020-03-22 01:47:55 +0700

    Update index.js

--- src/components/RoomHeader/index.js
+++ src/components/RoomHeader/index.js
@@ -13,9 +13,9 @@ export const RoomHeader = ({
     </button>
     <h1>{room.name && room.name.replace(user.id, '')}</h1>
     {room.users && (
-      <div>Log Out
+      <div>
         <svg>
-          <use xlinkHref="index.svg#remove" />
+          <use xlinkHref="index.svg#signout" />
         </svg>
       </div>
     )},
