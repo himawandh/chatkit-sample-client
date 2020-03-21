142a13fbb1d91b539f78b06006a0803aff99fbc7
Himawan Djaya Hendra <himawandh@gmail.com>
2020-03-21 21:26:12 +0700

    Update index.js

--- src/components/RoomHeader/index.js
+++ src/components/RoomHeader/index.js
@@ -11,12 +11,14 @@ export const RoomHeader = ({
         <use xlinkHref="index.svg#menu" />
       </svg>
     </button>
-    <h1>{room.name && room.name.replace(user.id, '')}</h1>      
+    <h1>{room.name && room.name.replace(user.id, '')}</h1>
+    {room.users && (
     <div>Log Out
         <svg>
           <use xlinkHref="index.svg#remove" />
         </svg>
-      </div>  
+      </div>
+    )}
     {room.users && (
       <div onClick={e => setUserList(!userListOpen)}>
         <span>{room.users.length}</span>
