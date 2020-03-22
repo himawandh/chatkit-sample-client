a50f38bd85a46373eb4e79c19eba4d7db5309e21
Himawan Djaya Hendra <himawandh@gmail.com>
2020-03-22 11:30:41 +0700

    Update auth.js

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -70,6 +70,6 @@ export default class Auth {
     localStorage.removeItem('expires_at')
     localStorage.removeItem('chatkit_user')
     //TODO: callback to reset the app state.
-    returnTo: window.location.origin
+    window.location.replace("https://obrolanku.netlify.com")
   }
 }
